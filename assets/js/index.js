d22afff38042f777747ccb5aeda76d07d4908741
AdonisJeronimo <adonisfjeronimo@gmail.com>
2023-02-15 13:30:59 -0300

    Estrutura do campo realizada

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+const canvasEl = document.querySelector("canvas"),
+canvasCtx = canvasEl.getContext("2d")
+
+const lineWidth = 15
+
+function setup() {
+canvasEl.width = canvasCtx.width = window.innerWidth
+canvasEl.height = canvasCtx.height = window.innerHeight
+
+}
+function draw() {
+canvasCtx.fillStyle = "#286047"
+canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight)
+
+
+canvasCtx.fillStyle = "#fff"
+canvasCtx.fillRect(window.innerWidth/2 - lineWidth / 2, 0 ,  lineWidth , window.innerHeight)
+
+
+}
+
+setup()
+draw()
