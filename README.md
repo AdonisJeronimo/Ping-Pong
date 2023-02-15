8dcd7638b482019f7633c84a00004840eaee94b8
AdonisJeronimo <adonisfjeronimo@gmail.com>
2023-02-15 18:33:14 -0300

    Implementação as das regras básicas

---
+++ README.md
@@ -0,0 +1,36 @@
+# PING-PONG| ![GitHub]( https://img.shields.io/badge/Status-%20development-orange)
+
+
+## Seja Bem-vindo(a) !!
+
+Obrigado por visitar meu perfil! :0 
+
+
+###  🚀 Projeto PING-PONG  🚀
+<br>
+
+O projeto PING-PONG tem como proposta desenvolver e aguçar a lógica de programação. Desenvolver jogos de alta ou baixa complexidade é uma exelente forma de melhorarmos nossa programação como um todo, porque é visualmente facil entender como as coisas funcionam.
+Neste projeto em especial, utilizaremos apenas HTML e JavaScript. 
+
+<div style="display: inline_block"> <br>
+<img height="****" width="***" src="****">
+  </div>
+
+
+
+### Habilidades utilizadas 💻👨🏻‍💻🖥
+
+<div style="display: inline_block"><br>
+  <ul>
+    <li> HTML <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> </li>
+   <li> CSS &nbsp;&nbsp; <img align="center" alt="CSS" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"></li>
+    </ul>
+</div>
+<br>
+
+ &nbsp;&nbsp;&nbsp;&nbsp; ![GitHub top language](https://img.shields.io/github/languages/top/AdonisJeronimo/Ping-Pong?style=plastic)
+
+<!-- ###  Processo de desenvolvimento ⌨️🖱
+
+Foi um desafio rápido de ser feito, pois exige apenas noções básicas de HTML e CSS, sendo assim, seu único e verdadeiro desafio é ser capaz de se atentar aos mínimos detalhes para que esta reprodução seja a mais próximo do original.
+Apenas foram utilizados arquivos de imagens como referência sem a presença de um layout no Figma ou qualquer outra ferramenta que nos possibilitasse ter acesso a as medidas exatas utilizadas neste projeto. -->
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,18 @@
 const canvasEl = document.querySelector("canvas"),
-    canvasCtx = canvasEl.getContext("2d")
-gapX = 10
+    canvasCtx = canvasEl.getContext("2d"),
+    gapX = 10
 
 const mouse = { x: 0, y: 0 }
 
-const lineWidth = 15
+// const lineWidth = 15
+
 const field = {
     w: window.innerWidth,
     h: window.innerHeight,
     draw: function () {
         canvasCtx.fillStyle = "#286047"
         canvasCtx.fillRect(0, 0, this.w, this.h)
-    }
+    },
 }
 
 const line = {
@@ -30,7 +31,6 @@ const leftPaddle = {
     h: 200,
     _move: function () {
         this.y = mouse.y - this.h / 2
-
     },
     draw: function () {
         canvasCtx.fillStyle = "#fff"
@@ -41,7 +41,7 @@ const leftPaddle = {
 }
 
 const rightPaddle = {
-    x: field.w - lineWidth - gapX,
+    x: field.w - line.w - gapX,
     y: 100,
     w: line.w,
     h: 200,
@@ -57,8 +57,14 @@ const rightPaddle = {
 }
 
 const score = {
-    human: 1,
-    computer: 2,
+    human: 0,
+    computer: 0,
+    increaseHuman: function () {
+        this.human++
+    },
+    increaseComputer: function () {
+        this.computer++
+    },
     draw: function () {
         canvasCtx.font = "bold 72px Arial"
         canvasCtx.textAlign = "center"
@@ -77,11 +83,37 @@ const ball = {
     directionX: 1,
     directionY: 1,
     _calcPosition: function () {
+        //verificação da pontuação do jogador 1
+        if (this.x > field.w - this.r - rightPaddle.w - gapX) {
+            //verifico se raquele direita esta na posição y da bola
+            if (this.y + this.r > rightPaddle.y &&
+                this.y - this.r < rightPaddle.y + rightPaddle.h
+            ) {  //reverte a bola invertendo o sinal de x
+                this._reverseX()
+            } else {
+                //pontuar jogador 1
+                score.increaseHuman()
+                this._pointUp()
+            }
+        }
+        //verificação da pontuação do jogador 2
+        if(this.x < this.r + leftPaddle.w + gapX ){
+            //verifica se a raquete esquerda esta na pisição y da bola
+            if(this.y  + this.r > leftPaddle.y && 
+                this.y - this.r < leftPaddle.y + leftPaddle.h
+                ){
+                //rebater a bola invertendo sinal de X
+                this._reverseX()
+            }else {
+                //pontuar jogador 2
+                score.increaseComputer()
+                this._pointUp()
+            }
+        }
         // verifica as laterias superiores e inferiores do campo
-        if (
-            (this.y - this.r < 0 && this.directionY < 0) ||
-            (this.y > field.h - this.r && this.directionY > 0)
-        ) {
+        if ((this.y - this.r < 0 && this.directionY < 0) || 
+        (this.y > field.h - this.r && this.directionY > 0)
+        ){
             // rebate a bola invertendo o sinal do eixo Y
             this._reverseY()
         }
@@ -92,6 +124,12 @@ const ball = {
     _reverseY: function () {
         this.directionY *= - 1
     },
+
+    _pointUp: function () {
+        this.x = field.w / 2
+        this.y = field.h / 2
+    },
+
     _move: function () {
         this.x += this.directionX * this.speed
         this.y += this.directionY * this.speed
@@ -106,7 +144,6 @@ const ball = {
         this._move()
     }
 }
-
 
 function setup() {
     canvasEl.width = canvasCtx.width = field.w
