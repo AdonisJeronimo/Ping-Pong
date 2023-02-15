863b198d52680717d3631fd695fa8bc56196cbcd
AdonisJeronimo <adonisfjeronimo@gmail.com>
2023-02-15 13:49:11 -0300

    Raquetes desenhadas

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,19 +3,29 @@ canvasCtx = canvasEl.getContext("2d")
 
 const lineWidth = 15
 
+
 function setup() {
 canvasEl.width = canvasCtx.width = window.innerWidth
 canvasEl.height = canvasCtx.height = window.innerHeight
 
 }
+
+// desenho do campo
+
 function draw() {
 canvasCtx.fillStyle = "#286047"
 canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight)
 
+// linha central
 
 canvasCtx.fillStyle = "#fff"
 canvasCtx.fillRect(window.innerWidth/2 - lineWidth / 2, 0 ,  lineWidth , window.innerHeight)
 
+// raquete esquerda
+canvasCtx.fillRect(10, 100, lineWidth, 200)
+
+// raquete direita
+canvasCtx.fillRect(window.innerWidth - lineWidth - 10, 300 ,  lineWidth, 200)
 
 }
 
